5e922154806e51c070d4e58250d38f3dea953966
butler-oliver <oliverbutler15018@gmail.com>
2019-11-18 15:08:38 +0000

    Checks if the username used to login exists in the system

---
+++ username.py
@@ -0,0 +1,12 @@
+# Checks if the username used to login exists in the system.
+username = input("Login: >> ")
+
+user1 = "Boo"
+user2 = "mer"
+
+if username == user1:
+    print ("Access granted")
+elif username == user2:
+    print ("Welcome to the system")
+else:
+    print ("Access denied")
