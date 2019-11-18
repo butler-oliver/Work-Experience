4b8c818ff6d87e0804e57c86f4b2fb673efd341c
butler-oliver <oliverbutler15018@gmail.com>
2019-11-18 11:51:17 +0000

    k

---
+++ README.py
@@ -0,0 +1,2 @@
+# Work-Experience
+print('Python Script !')
--- Work.py
+++ Work.py
@@ -1,1 +1,21 @@
-print("hello world")
+def add(num1,num2):
+    anadd = num1 + num2
+    return anadd
+def sub(num1,num2):
+    ansub = num1 - num2
+    return ansub
+def mul(num1,num2):
+    anmul = num1 * num2
+    return anmul
+def div(num1,num2):
+    andiv = num1/num2
+    return andiv
+
+
+
+num1 = int(input("number"))
+num2 = int(input("number"))
+print(add(num1,num2))
+print(sub(num1,num2))
+print(mul(num1,num2))
+print(div(num1,num2))
